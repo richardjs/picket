f42b46ca3356f5b3b33559f64475069121bf0dc1
Richard Schneider <rschneider@harding.edu>
2018-01-10 15:59:24 -0600

    change generate_bitboards.y to define two different sets of moves, for normal and capturing

--- generate_bitboards.py
+++ generate_bitboards.py
@@ -1,13 +1,13 @@
-print('const bitboard MOVES[2][64] = {{')
+print('const bitboard STRAIGHT_MOVES[2][64] = {{')
 for row in range(7):
 	right = ('0' * 8 * (row + 1)) + ','
 	left = '0' * 8 * (6 - row)
 	left = '0b' + left
 	left = '\t' + left
-	print(left + '00000011' + right)
+	print(left + '00000001' + right)
 	for col in range(6):
-		print(left + ('0'*(5-col)) + '111' + ('0'*col) + right)
-	print(left + '11000000' + right)
+		print(left + ('0'*(5-col)) + '010' + ('0'*col) + right)
+	print(left + '10000000' + right)
 
 for col in range(8):
 	if col == 7:
@@ -25,11 +25,47 @@ for row in range(7):
 	left = '0' * 8 * (7 - row)
 	left = '0b' + left
 	left = '\t' + left
-	print(left + '00000011' + right)
+	print(left + '00000001' + right)
 	for col in range(6):
-		print(left + ('0'*(5-col)) + '111' + ('0'*(col)) + right)
+		print(left + ('0'*(5-col)) + '010' + ('0'*(col)) + right)
 	if row == 6:
-		print(left + '11000000' + right[:-1])
+		print(left + '10000000' + right[:-1])
 	else:
-		print(left + '11000000' + right)
+		print(left + '10000000' + right)
+print('}};')
+
+print('const bitboard DIAGONAL_MOVES[2][64] = {{')
+for row in range(7):
+	right = ('0' * 8 * (row + 1)) + ','
+	left = '0' * 8 * (6 - row)
+	left = '0b' + left
+	left = '\t' + left
+	print(left + '00000010' + right)
+	for col in range(6):
+		print(left + ('0'*(5-col)) + '101' + ('0'*col) + right)
+	print(left + '01000000' + right)
+
+for col in range(8):
+	if col == 7:
+		print('\t0b' + '0' * 64)
+	else:
+		print('\t0b' + '0' * 64 + ',')
+
+print('}, {')
+
+for col in range(8):
+	print('\t0b' + '0' * 64 + ',')
+
+for row in range(7):
+	right = ('0' * 8 * (row)) + ','
+	left = '0' * 8 * (7 - row)
+	left = '0b' + left
+	left = '\t' + left
+	print(left + '00000010' + right)
+	for col in range(6):
+		print(left + ('0'*(5-col)) + '101' + ('0'*(col)) + right)
+	if row == 6:
+		print(left + '01000000' + right[:-1])
+	else:
+		print(left + '01000000' + right)
 print('}};')
